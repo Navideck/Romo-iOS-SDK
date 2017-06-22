bb8c882777c2584d8ae8b42533517c79a3699383
Foti Dim <foti@navideck.com>
2017-06-22 10:46:48 +0200

    Fixed RMShared headers

--- SDK/RMShared/RMShared/RMShared.h
+++ SDK/RMShared/RMShared/RMShared.h
@@ -1,19 +1,7 @@
-//
-//  RMShared.h
-//  RMShared
-//
-//  Created by Foti Dim on 21.06.17.
-//  Copyright © 2017 Romotive, Inc. All rights reserved.
-//
+#import <Foundation/Foundation.h>
 
-#import <UIKit/UIKit.h>
-
-//! Project version number for RMShared.
-FOUNDATION_EXPORT double RMSharedVersionNumber;
-
-//! Project version string for RMShared.
-FOUNDATION_EXPORT const unsigned char RMSharedVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <RMShared/PublicHeader.h>
-
+#import "RMCircleMath.h"
+#import "RMMath.h"
+#import "RMQuaternion.h"
+#import "RMDispatchTimer.h"
 
